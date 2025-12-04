e0e8cb75a26a1dcf65c6bc2f7fd6e5df73be03b4
javiergarciabermejo <145556743+javiergarciabermejo@users.noreply.github.com>
2025-12-04 12:34:06 +0100

    Update translations.js

--- app/public/translations.js
+++ app/public/translations.js
@@ -100,16 +100,7 @@ const translations = {
         writeNotes: "Escribe tus notas...",
         new: "Nuevo",
 
-//comparativa 
 
-        comparison = "Comparativa",
-income = "Ingresos",
-expenses = "Gastos",
-year = "Año",
-month = "Mes",
-selectYears = "Seleccionar años",
-selectMonths = "Seleccionar meses",
-categories = "Categorías",
 
 
   //CONSEJOS- PERSONALIZADOS 
@@ -370,16 +361,7 @@ categories = "Categorías",
         writeNotes: "Write your notes...",
         new: "New",
 
-//comparativa
 
-        comparison = "Comparison",
-income = "Income",
-expenses = "Expenses",
-year = "Year",
-month = "Month",
-selectYears = "Select years",
-selectMonths = "Select months",
-categories = "Categories",
     
   //consejos personalizados 
         "back": "← Back to Tips",
